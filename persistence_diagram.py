129d5de6d718df83528357935cda99a793dfb6ff
mlamczyk <lamczykmagdalena@gmail.com>
2025-08-30 16:25:31 +0200

    Added Ripser

--- persistence_diagram.py
+++ persistence_diagram.py
@@ -1,7 +1,26 @@
 import math
+import numpy as np
 import gudhi as gd
+from ripser import Rips
 import matplotlib.pyplot as plt
 
+
+# Ripser
+# Diagram persystencji filtracji losowej chmury punktów
+rips = Rips()
+data = np.random.random((100, 2))
+diagrams = rips.fit_transform(data)
+
+# Rysowanie diagramu persystencji
+fig, ax = plt.subplots(figsize=(6, 6))
+rips.plot(diagrams, ax=ax)
+plt.title("Diagram persystencji")
+plt.savefig("diagram1.pdf", bbox_inches='tight')
+plt.show()
+plt.close()
+
+# ----------------------------------------------------------
+# Gudhi
 
 # Punkty persystencji w formacie (dimension, (birth, death))
 persistence_pairs = [
